bee6959b1e2dec549bee4639fc943bab40cfd536
Martin <Martin@Martin>
2013-10-26 21:26:32 +0200

    Complete new start of this project.

--- NetworkChat/src/NetworkChat/NetworkChat.java
+++ NetworkChat/src/NetworkChat/NetworkChat.java
@@ -2,18 +2,10 @@ package NetworkChat;
 
 import org.apache.log4j.BasicConfigurator;
 
-import Connection.IConnection;
-import Connection.impl.Connection;
-
 public class NetworkChat {
 	
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
-		IConnection con = new Connection("localhost",12345);
-		con.Connect();	
-	    
-		while(con.isConnected()){  
-			
-		}
+
 	}
 }
